e8d7cd82e21ba0d08dc51382b3e6f24c325814c0
Jungyong Seo <jung-yong.seo12@imperial.ac.uk>
2016-03-23 16:40:49 +0000

    Ini,,,

---
+++ Source/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Simulation
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("..");
+        }
+    }
+}
