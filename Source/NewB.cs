115a467c439fa88fde247df6c80fec601fe8b86b
Jungyong Seo <jung-yong.seo12@imperial.ac.uk>
2016-03-23 16:44:20 +0000

    property added

--- Source/NewB.cs
+++ Source/NewB.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Simulation
+﻿namespace Simulation
 {
     class NewB
     {
+        public string ID { get; set; }
     }
 }
